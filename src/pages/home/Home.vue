fab5fbefd255480a381f94882b8a084d51977b11
One Piece <a809653361@icloud.com>
2021-04-02 17:06:09 +0800

    框架底层的封装还没做，回去做一下吧，get/post/token拦截器等底层的封装

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -11,9 +11,9 @@
       <home-center></home-center>
     </div>
     <div class="center-right">
-<home-cube :title="titlelist[0]" :list="list" :urls="urls" :listurl="listurl"></home-cube>
+<home-cube :title="titlelist[2]" :list="list" :urls="urls" :listurl="listurl"></home-cube>
       <div class="xx">xx</div>
-      <home-cube :title="titlelist[1]" :list="list" :urls="urls" :listurl="listurl"></home-cube>
+      <home-cube :title="titlelist[3]" :list="list" :urls="urls" :listurl="listurl"></home-cube>
     </div>
     <home-bottom></home-bottom>
     <creater-info></creater-info>
@@ -27,7 +27,7 @@ import HomeCube from "./components/SmallCube";
 import HomeCenter from "./components/HomeCenter";
 import HomeBottom from './components/HomeBottom';
 import CreaterInfo from './components/CreaterInfo';
-
+import axios from 'axios'; 
 export default {
   name: "Home",
   components: {
@@ -39,13 +39,51 @@ export default {
     CreaterInfo
   },
   data() {
+    //参考之前的代码写的逻辑，hhh，重构的难度还是无语呀
     return {
-      titlelist: ["公告", "视频"],
-      list: ["内很方便", "内容2", "内容3","内容1", "内容2", "内容3","内容1", "内容2", "内容3"],
+      titlelist: ["公告", "好人好事","公益链接","公益活动"], //title
+      announcementlist: [],  //公告集合
+      publicconnect:[],      //公益链接集合
+      goodthings:[],         //好人好事集合
+      publicactivity:[],     //公益活动集合
       urls:["//www.baidu.com","#","#"],
       listurl:"#"
     };
   },
+  methods:{
+    //初始化函数
+    init(){},
+    //获取titlelist，其实不用获取也行..(就在data里面写死了)
+    getTitle(){return null;},
+    //获取公告的内容list,此处没有封装好底层post和get的函数，而且用户的token也没有考虑
+    getAnnouncementList(){
+      axios.get('api地址',{params:{}})
+      .then()
+      .catch(error=>{console.log(error)})
+    },
+    //公益超链接内容
+    getPublicconnectList(){
+       axios.get('api地址',{params:{}})
+      .then()
+      .catch(error=>{console.log(error)})
+    },
+    //好人好事内容
+    getGoodthingsList(){
+       axios.get('api地址',{params:{}})
+      .then()
+      .catch(error=>{console.log(error)})
+    },
+    //活动内容
+    getPublicactivityList(){
+       axios.get('api地址',{params:{}})
+      .then()
+      .catch(error=>{console.log(error)})
+    }
+
+  },
+  mounted(){
+      this.init()
+  }
 };
 </script>
 
