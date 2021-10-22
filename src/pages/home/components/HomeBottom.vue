17fdde35585b6608cdcbfba052bc743d7c44d5ea
One Piece <a809653361@icloud.com>
2021-10-22 09:38:18 +0800

    test push

--- src/pages/home/components/HomeBottom.vue
+++ src/pages/home/components/HomeBottom.vue
@@ -2,7 +2,7 @@
   <div>
     <footer>
       <div class="bottom-footer">
-        <a href="#" >返回顶部</a>
+        <a href="#">返回顶部</a>
         |
         <a href="#">关于我们</a>
         |
@@ -28,27 +28,26 @@ export default {
 <style>
 /*底层相关信息*/
 .bottom-footer {
-    float: left;
-    margin-top: 20px;
-    width: 100%;
-    height: 30px;
-    background-color: gray;
-    text-align: center;
-  }
-  
-  .bottom-footer a {
-    font-size: 23px;
-    padding-left: 20px;
-    padding-right: 15px;
-    padding-top: 15px;
-    /* margin-top: 0px; */
-    color: aliceblue;
-  }
-  .bottom-footer a:hover {
-    color: #ff702d;
-    text-decoration-line: underline; /*修改为有下划线*/
-    text-decoration-style: solid; /*设置为默认*/
-    text-decoration-color: currentcolor; /*修改下划线颜色*/
-  }
+  float: left;
+  margin-top: 20px;
+  width: 100%;
+  height: 30px;
+  background-color: gray;
+  text-align: center;
+}
 
+.bottom-footer a {
+  font-size: 23px;
+  padding-left: 20px;
+  padding-right: 15px;
+  padding-top: 15px;
+  /* margin-top: 0px; */
+  color: aliceblue;
+}
+.bottom-footer a:hover {
+  color: #ff702d;
+  text-decoration-line: underline; /*修改为有下划线*/
+  text-decoration-style: solid; /*设置为默认*/
+  text-decoration-color: currentcolor; /*修改下划线颜色*/
+}
 </style>
